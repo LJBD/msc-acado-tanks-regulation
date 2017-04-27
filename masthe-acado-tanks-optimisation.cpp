de8880994cc5a25a29c9110600b44dd10fb23c11
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-04-27 13:12:39 +0200

    Initial commit.

---
+++ masthe-acado-tanks-optimisation.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : masthe-acado-tanks-optimisation.cpp
+// Author      : Lukasz Dudek
+// Version     :
+// Copyright   : LGPL
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("Hello World!!!");
+	return EXIT_SUCCESS;
+}

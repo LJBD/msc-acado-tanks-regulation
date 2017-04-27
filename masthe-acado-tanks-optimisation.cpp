fdd22342b93321022b779952107813e69488bb2b
Lukasz Dudek <lukasz.j.dudek@uj.edu.pl>
2017-04-27 15:51:10 +0200

    Implemented the tanks' model in ACADO.

--- masthe-acado-tanks-optimisation.cpp
+++ masthe-acado-tanks-optimisation.cpp
@@ -8,6 +8,7 @@
 
 #include <acado_optimal_control.hpp>
 #include <acado_gnuplot.hpp>
+#include "tanks_constants.hpp"
 
 
 int main( ){
@@ -20,7 +21,8 @@ int main( ){
     // INTRODUCE THE VARIABLES:
     // -------------------------
 
-    DifferentialState     s,v,m;
+//    DifferentialState     s,v,m;
+    DifferentialState h1, h2, h3;
     Control               u    ;
     Parameter             T    ;
 
@@ -30,36 +32,42 @@ int main( ){
     // DEFINE A DIFFERENTIAL EQUATION:
     // -------------------------------
 
-    f << dot(s) == v;
-    f << dot(v) == (u-0.2*v*v)/m;
-    f << dot(m) == -0.01*u*u;
+//    f << dot(s) == v;
+//    f << dot(v) == (u-0.2*v*v)/m;
+//    f << dot(m) == -0.01*u*u;
+    f << dot(h1) == (u - C1*sqrt(h1))/a*w;
+    f << dot(h2) == (C1*sqrt(h1) - C2*sqrt(h2))/(c*w + h2*b*w/h_max);
+    f << dot(h3) == (C2*sqrt(h2) - C3*sqrt(h3))/(w*sqrt(pow(R, 2) - pow((R - h3), 2)));
 
 
     // DEFINE AN OPTIMAL CONTROL PROBLEM:
     // ----------------------------------
-    OCP ocp( 0, T, 20 );
+    OCP ocp( 0, T, 50 );
 
     ocp.minimizeMayerTerm( T );
     ocp.subjectTo( f );
 
-    ocp.subjectTo( AT_START, s ==  0.0 );
-    ocp.subjectTo( AT_START, v ==  0.0 );
-    ocp.subjectTo( AT_START, m ==  1.0 );
+    ocp.subjectTo( AT_START, h1 ==  h10 );
+    ocp.subjectTo( AT_START, h2 ==  h20 );
+    ocp.subjectTo( AT_START, h3 ==  h30 );
 
-    ocp.subjectTo( AT_END  , s == 10.0 );
-    ocp.subjectTo( AT_END  , v ==  0.0 );
+    ocp.subjectTo( AT_END  , h1 == h1_final);
+    ocp.subjectTo( AT_END  , h2 == h2_final);
+    ocp.subjectTo( AT_END  , h3 == h3_final);
 
-    ocp.subjectTo( -0.1 <= v <=  1.7  );
-    ocp.subjectTo( -1.1 <= u <=  1.1  );
-    ocp.subjectTo(  5.0 <= T <= 15.0  );
+    ocp.subjectTo( 0 <= h1 <=  h_max  );
+    ocp.subjectTo( 0 <= h2 <=  h_max  );
+    ocp.subjectTo( 0.001 <= h3 <=  h_max  ); //to avoid hitting singularity
+    ocp.subjectTo( 0 <= u <=  u_max  );
+//    ocp.subjectTo(  5.0 <= T <= 15.0  );
 
 
     // VISUALIZE THE RESULTS IN A GNUPLOT WINDOW:
     // ------------------------------------------
     GnuplotWindow window;
-        window.addSubplot( s, "THE DISTANCE s"      );
-        window.addSubplot( v, "THE VELOCITY v"      );
-        window.addSubplot( m, "THE MASS m"          );
+        window.addSubplot( h1, "1st tank's level"      );
+        window.addSubplot( h2, "2nd tank's level"      );
+        window.addSubplot( h3, "3rd tank's level"          );
         window.addSubplot( u, "THE CONTROL INPUT u" );
 
 
@@ -67,7 +75,7 @@ int main( ){
     // ---------------------------------------------------
     OptimizationAlgorithm algorithm(ocp);
 
-    algorithm.set( MAX_NUM_ITERATIONS, 20 );
+    algorithm.set( MAX_NUM_ITERATIONS, 200 );
 // 	algorithm.set( HESSIAN_APPROXIMATION, EXACT_HESSIAN );
 // 	algorithm.set( HESSIAN_PROJECTION_FACTOR, 1.0 );
 
---
+++ tanks_constants.hpp
@@ -0,0 +1,32 @@
+/*
+ * tanks_constants.hpp
+ *
+ *  Created on: 27 Apr 2017
+ *      Author: stageeyesrv
+ */
+
+#ifndef TANKS_CONSTANTS_HPP_
+#define TANKS_CONSTANTS_HPP_
+
+    float a = 31.0;
+    float b = 40.0;
+    float c = 10.0;
+    float R = 40.0;
+    float w = 5.0;
+    float h_max = 40.0;
+
+    float C1 = 12.0;
+    float C2 = 12.0;
+    float C3 = 12.0;
+
+    float h10 = 10.0;
+    float h20 = 10.0;
+    float h30 = 10.0;
+
+    float u_max = 20.0;
+
+    float h1_final = 5.0;
+    float h2_final = 5.0;
+    float h3_final = 5.0;
+
+#endif /* TANKS_CONSTANTS_HPP_ */
